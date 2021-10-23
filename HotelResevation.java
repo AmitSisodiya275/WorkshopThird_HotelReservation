1ded43b46e89e59711d65935aaa69ee44014ab37
AmitSisodiya275 <amitsisodiya275@gmail.com>
2021-10-23 08:36:56 +0530

    Added: a new file which can print welcome meddage on the console.

---
+++ HotelResevation.java
@@ -0,0 +1,10 @@
+package com.bridgelab.hotelsystem;
+
+public class HotelResevation {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Hotel Reservation Program.");
+	}
+
+}
